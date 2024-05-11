6f147f31863fcb993d8d903d778b6dc873275001
RM <rmm>
2024-05-11 16:35:14 +0200

    Character model and initial data

--- webservice/api/models.py
+++ webservice/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class Character(models.Model):
+    name = models.CharField(max_length=64, help_text=_('e.g.: John Doe'))
+    origin = models.CharField(max_length=64, blank=True, help_text=_('e.g.: Some movie'))
+
+    @property
+    def description(self):
+        if (len(self.origin) > 0):
+            return f'{self.name} ({self.origin})'
+        return self.name
+
+    def __str__(self):
+        return self.description
--- webservice/client/views.py
+++ webservice/client/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello from the client django view. Check out <a href="/api">The browsable API</a>')
+    return HttpResponse('''
+    <p>Hello from the client django view.</p>
+    <p>Check out <a href="/api">The browsable API</a></p>
+    <p>Or the <a href="/admin">Admin</a></p>
+    ''')
